00ba87131cd8718d51651f673552a82dd00a85a9
end <lihuinihao6315@sina.cn>
2017-04-11 15:34:40 +0800

    -lh add files

--- configserver/src/main/java/com/microservice/ConfigserverApplication.java
+++ configserver/src/main/java/com/microservice/ConfigserverApplication.java
@@ -3,17 +3,14 @@ package com.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@EnableAutoConfiguration
-//@EnableDiscoveryClient
 @EnableConfigServer
+//@EnableDiscoveryClient
+@SpringBootApplication
 public class ConfigserverApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigserverApplication.class, args);
 	}
